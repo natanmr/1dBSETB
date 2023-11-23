b7befbfaf830212b812046d46a30157682d7bb3c
natanmr <47799038+natanmr@users.noreply.github.com>
2023-11-23 20:54:09 -0300

    Create main.jl

---
+++ main.jl
@@ -0,0 +1,40 @@
+##################################################
+### Title: 1DBSETB                             ###
+### Description                                ###
+### Author: Natan Moreira Regis                ###
+### Contact: natan.moreira.regis12@gmail.com   ###
+### Last Update: 23 nov 2023                   ###
+### Licence: GLPv3                             ###
+##################################################
+
+##  READ input:
+function Read_input()
+
+end
+
+## READ TB Hamiltonian:
+function read_TB_Hamiltonian()
+
+end
+
+## Calculate optical properties:
+function optics():
+
+end
+
+
+## BSE Solver:
+function BSE():
+
+end
+
+
+## Write Outputs:
+function Write_Output():
+
+
+  
+end
+
+
+
